ec74e403a29ea51c78999cc0f33e2eec00df5473
Val <dgm97@cornell.edu>
2019-03-24 13:58:15 -0400

    change port serving api

--- api/src/bin/www.js
+++ api/src/bin/www.js
@@ -4,7 +4,7 @@ import http from 'http';
 
 const debug = debugLib('api:server');
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
 const server = http.createServer(app);
