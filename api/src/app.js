839b6afa3e70c7b542dde7a05b07c6783e010f7b
Val <dgm97@cornell.edu>
2019-03-24 17:41:45 -0400

    configure cors

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,13 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors());
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: whitelist,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
