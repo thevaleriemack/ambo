78cff705b80c542045e9140a5acd3cd05027b367
Val <dgm97@cornell.edu>
2019-04-15 19:06:51 -0400

    fix undefined error by checking for null

--- client/src/store/assets/index.js
+++ client/src/store/assets/index.js
@@ -14,7 +14,7 @@ export function getAllAssets(networkId=1) {
         return null;
       })
       .then((res) => {
-        return res.data;
+        return res && res.data;
       });
 
     if (assets === null) {
